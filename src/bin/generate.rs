31fc6a448ca098b534c6f02116cc2c48fb69c926
Jeff Schwab <jeff@unbuggy.com>
2020-11-20 21:10:01 -0500

    Generate self-signed root cert

--- src/bin/generate.rs
+++ src/bin/generate.rs
@@ -1,15 +1,8 @@
-use core::fmt::Debug;
-
 use std::env;
-use std::ffi::OsStr;
-use std::path::Path;
 use std::process;
 use std::process::Command;
 
-fn openssl<S>(args: &[S]) -> Result<(), String>
-where
-    S: AsRef<OsStr> + Debug,
-{
+fn openssl(args: &[&str]) -> Result<(), String> {
     match Command::new("openssl").args(args).status() {
         Ok(status) => {
             if status.success() {
@@ -22,34 +15,41 @@ where
     }
 }
 
-fn main() {
-    let argv: Vec<_> = env::args_os().skip(1).collect();
-    if !(1..2).contains(&argv.len()) {
-        eprintln!("usage: generate <ca> [server]");
-        process::exit(2);
-    }
-
-    let ca_name = Path::new(&argv[0]);
-    let ca_key = ca_name.with_extension("key");
-
-    // There's no good reason file paths should ever be represented as &str
-    // rather than &OsStr or &Path, except that only &str supports literal
-    // syntax, and I'll be damned if I'm going to junk up every argument to
-    // every subprocess with OsStr::new().
-    let ca_key = ca_key
-        .to_str()
-        .expect("name must be representable in UTF-8");
-
-    #[rustfmt::skip]
-    let command = [
+#[rustfmt::skip]
+fn generate_ca(name: &str) -> Result<(), String> {
+    let key = format!("{}.key", name);
+    let pem = format!("{}.pem", name);
+    openssl(&[
         "genpkey",           // Generate a private key.
-        "-out", &ca_key,     // output file name
+        "-out", &key,        // output file name
         "-outform", "PEM",   // output format
         "-algorithm", "RSA", // algorithm to generate the key
         "-aes-256-cbc",      // algorithm to encrypt the key
-    ];
+    ])?;
+    openssl(&[
+        "req", "-new", "-x509", // Request a new X.509 certificate.
+        "-key", &key,           // public key to be signed (and private key to sign with)
+        "-sha256",              // message digest to sign the request
+        "-out", &pem,           // output file name
+        "-days", "7300",        // how long before the cert expires
+    ])
+}
 
-    if let Err(err) = openssl(&command) {
+#[allow(unused_variables)]
+fn generate_server(name: &str, ca_name: &str) -> Result<(), String> {
+    todo!()
+}
+
+fn main() {
+    let argv: Vec<_> = env::args().skip(1).collect();
+    if let Err(err) = match argv.len() {
+        1 => generate_ca(&argv[0]),
+        2 => generate_server(&argv[1], &argv[0]),
+        _ => {
+            eprintln!("usage: generate <ca> [server]");
+            process::exit(2);
+        }
+    } {
         eprintln!("error: {}", err);
         process::exit(1);
     }
